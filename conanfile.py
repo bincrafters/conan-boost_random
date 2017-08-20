1a6c320148317f2a23eb6d13cff620c639895413
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:45:14 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,7 @@ class BoostRandomConan(ConanFile):
     version = "1.64.0"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-random"
     source_url = "https://github.com/boostorg/random"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -28,7 +29,7 @@ class BoostRandomConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def build(self):
