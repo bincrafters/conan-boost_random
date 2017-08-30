a765af5a1ed9f5b7f59e486399bd7e0f75690658
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 01:59:49 -0400

    add shared option change source method append BOOST_ALL_NO_LIB

--- conanfile.py
+++ conanfile.py
@@ -7,10 +7,11 @@ class BoostRandomConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-random"
-    source_url = "https://github.com/boostorg/random"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["random"]
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
     requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
                       "Boost.Config/1.64.0@bincrafters/testing", \
@@ -28,20 +29,23 @@ class BoostRandomConan(ConanFile):
                       #assert1 config0 core2 integer3 math8 mpl5 range7 static_assert1 system3 throw_exception2 type_traits3 utility5
 
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + version        
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def build(self):
         self.run(self.deps_user_info['Boost.Generator'].b2_command)
-        
+
     def package(self):
+        self.copy(pattern="*", dst="lib", src="stage/lib")
         for lib_short_name in self.lib_short_names:
             include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)		
-
-        self.copy(pattern="*", dst="lib", src="stage/lib")
+            self.copy(pattern="*", dst="include", src=include_dir)
 
     def package_info(self):
-        self.user_info.lib_short_names = (",").join(self.lib_short_names)
+        self.user_info.lib_short_names = ",".join(self.lib_short_names)
         self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.defines.append("BOOST_ALL_NO_LIB=1")
