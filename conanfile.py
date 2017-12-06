94b446be28fc42bb8b40e4988bf4fe92b2549b18
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:48 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,10 +4,10 @@ from conans import ConanFile, tools
 class BoostRandomConan(ConanFile):
     name = "Boost.Random"
     version = "1.65.1"
-    settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
         "Boost.Assert/1.65.1@bincrafters/testing", \
         "Boost.Config/1.65.1@bincrafters/testing", \
         "Boost.Core/1.65.1@bincrafters/testing", \
@@ -27,22 +27,18 @@ class BoostRandomConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-random"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
     generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
-    def package_id(self):
-        if self.is_header_only:
-            self.info.header_only()
-
-    # pylint: disable=unused-import
     @property
     def env(self):
         try:
             with tools.pythonpath(super(self.__class__, self)):
-                import boostgenerator # pylint: disable=F0401
+                import boostgenerator  # pylint: disable=F0401
                 boostgenerator.BoostConanFile(self)
         except:
             pass
