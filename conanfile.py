7e16c27a82c5870203a8dcdb68af4f94ad069b41
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:34 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,22 +3,22 @@ from conans import ConanFile
 
 class BoostRandomConan(ConanFile):
     name = "Boost.Random"
-    version = "1.65.1"
+    version = "1.66.0"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Integer/1.65.1@bincrafters/testing", \
-        "Boost.Math/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Range/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.System/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Utility/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Integer/1.66.0@bincrafters/testing", \
+        "Boost.Math/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Range/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.System/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Utility/1.66.0@bincrafters/testing"
 
     lib_short_names = ["random"]
     is_header_only = False
@@ -26,9 +26,9 @@ class BoostRandomConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-random"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
