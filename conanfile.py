b0d974bc7476ad9da738297de81db3853f6e6a02
Jerry W <jerrywiltse@gmail.com>
2017-12-31 18:18:34 -0500

    Update url field in conanfile [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, tools
 class BoostRandomConan(ConanFile):
     name = "boost_random"
     version = "1.66.0"
-    url = "https://github.com/bincrafters/conan-boost-random"
+    url = "https://github.com/bincrafters/conan-boost_random"
     
     lib_short_names = ["random"]
     is_header_only = False
