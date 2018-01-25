4b51e2c75a0d1728b5caaecdc792980aa6f851ec
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:30 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostRandomConan(ConanFile):
     name = "boost_random"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_random"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["random"]
     is_header_only = False
 
