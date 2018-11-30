e29c8df7698e9838eb69cb27a6d9bc3e678bd5e7
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,28 +4,13 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostRandomConan(base.BoostBaseConan):
     name = "boost_random"
     url = "https://github.com/bincrafters/conan-boost_random"
     lib_short_names = ["random"]
+    cycle_group = "boost_cycle_group_c"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_core",
-        "boost_integer",
-        "boost_io",
-        "boost_math",
-        "boost_mpl",
-        "boost_range",
-        "boost_static_assert",
-        "boost_system",
-        "boost_throw_exception",
-        "boost_type_traits",
-        "boost_utility"
-    ]
-
-
+    b2_requires = ["boost_cycle_group_c"]
