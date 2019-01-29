0309ebadfed7a2f940d3a5f182910ef729eec82b
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:22 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostRandomConan(base.BoostBaseConan):
     name = "boost_random"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_random"
     lib_short_names = ["random"]
     cycle_group = "boost_cycle_group_c"
